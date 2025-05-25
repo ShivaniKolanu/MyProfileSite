3d0fa7790d5ddd294412d80673a44a02bc7d4501
Shivani Kolanu <shivanikolanu2000@gmail.com>
2025-05-25 00:25:40 -0400

    few changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/{MyProfileSite}/',
+  base: '/MyProfileSite/',
   plugins: [react(),tailwindcss(),
   ],
 })
